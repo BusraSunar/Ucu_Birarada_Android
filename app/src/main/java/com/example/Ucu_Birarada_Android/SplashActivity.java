18d7c0f153e8e7bb52cba14f3560352aa737b20e
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2022-04-16 03:09:48 +0300

    first commit

---
+++ app/src/main/java/com/example/Ucu_Birarada_Android/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.Ucu_Birarada_Android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
